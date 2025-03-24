d3ad1632573a6acb4b65a766784c8a26b7e6451a
Naomi <nsano1@asu.edu>
2025-03-24 13:40:37 -0700

    added multiple model testing

--- main/config.py
+++ main/config.py
@@ -1,4 +1,4 @@
-file_path = "data/custom_dataset.csv"
+file_path = "data/github_custom_dataset.csv"
 irr_col_name = "Prompt with irrelevant context"
 rel_col_name = "Prompt without irrelevant context"
 answer_col_name = "Correct answer"
